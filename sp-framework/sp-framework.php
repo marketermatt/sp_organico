2a49d454c15df8c3e92889e2e6f14cd4286e41f8
gaganphp <mailtogagan990@gmail.com>
2015-09-25 23:14:25 +0530

    Removed License API
    Removed License API

--- sp-framework/sp-framework.php
+++ sp-framework/sp-framework.php
@@ -94,9 +94,4 @@ require_once( get_template_directory() . '/sp-framework/functions/control-panel-
 // load backend control panel
 require_once( get_template_directory() . '/sp-framework/functions/control-panel.php' );
 	
-/////////////////////////////////////////////////////
-// include API manager code
-/////////////////////////////////////////////////////
-require_once( 'am/function.php' );
-	
 ?>
